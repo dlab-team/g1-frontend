602e0a1553d1221b76358e3d2389537ad829b90a
Gerardo Santana <gerardosantana2013@gmail.com>
2024-08-20 17:17:36 -0400

    Elimina archivos temporales de prueba

--- src/routes/RoutesApp.jsx
+++ src/routes/RoutesApp.jsx
@@ -1,11 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import NotFoundPage from "../pages/NotFoundPage";
-import Index from "../pages/Index";
 
 const RoutesApp = () => {
   return (
     <Routes>
-        <Route path="/" element={<Index/>}></Route>
         <Route path="*" element={<NotFoundPage/>}></Route>
     </Routes>
   )
