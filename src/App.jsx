6e7fbcebd8eed55f9cc8c629c3222db7b3150f94
Manuel.Millan1979 <nutriglar@gmail.com>
2024-08-24 08:43:16 -0400

    elimina imagenes de iconos

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
 import ReactDOM from 'react-dom';
-import ResetPassword from "./components/ResetPassword";
-import Modal from "./components/Modal";
 import React, { useState } from "react";
 import './index.css';
-ReactDOM.render(<App />, document.getElementById('root'));
 
 function App() {
-  const [isModalVisible, setModalVisible] = useState(false);
-
-  const handleResetPasswordSubmit = () => {
-    setModalVisible(true);
   };
 
   return (
     <div>
-      {!isModalVisible ? (
-        <ResetPassword onSubmit={handleResetPasswordSubmit}/>
-      ) : (
-        <Modal />
-      )}
+    <h1></h1>
     </div>
   );
-}
+
 
 export default App;
