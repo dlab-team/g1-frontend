aa28d31b1e6d5cfd548b20a23a3fbd44f85c5252
fernandosbenavides <fernandosbenavides@gmail.com>
2024-09-09 22:39:12 -0300

    Conexion de Login y Register con el Backend

--- src/components/Login/FormLogin.jsx
+++ src/components/Login/FormLogin.jsx
@@ -1,147 +1,170 @@
-import { useState, useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-import ToggleSwitch from "./ToggleSwitch"
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import ToggleSwitch from "./ToggleSwitch";
 
 const Login = () => {
-    const ENDPOINT = "http://localhost:5173/login"
+  const ENDPOINT = "http://localhost:3000/login";
 
-    const initialFormState = {
-        email: '',
-        password: '',
+  const initialFormState = {
+    email: "",
+    password: "",
+  };
+
+  const [user, setUser] = useState(initialFormState);
+  const [rememberPassword, setRememberPassword] = useState(false);
+  const [countPass, setCountPass] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const savedEmail = localStorage.getItem("email");
+    const savedPassword = localStorage.getItem("password");
+
+    if (savedEmail && savedPassword) {
+      setUser({ email: savedEmail, password: savedPassword });
+      setRememberPassword(true);
+    }
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === "password") {
+      setCountPass(value.length < 8);
     }
 
-    const [user, setUser] = useState(initialFormState)
-    const [rememberPassword, setRememberPassword] = useState(false)
-    const [countPass, setCountPass] = useState(false)
-    const navigate = useNavigate()
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+  };
 
-    useEffect(() => {
-        const savedEmail = localStorage.getItem('email')
-        const savedPassword = localStorage.getItem('password')
-        
-        if (savedEmail && savedPassword) {
-            setUser({ email: savedEmail, password: savedPassword })
-            setRememberPassword(true)
-        }
-    }, [])
+  const handleToggleChange = (newValue) => {
+    setRememberPassword(newValue);
 
-    const handleInputChange = (e) => { 
-        const { name, value } = e.target
+    if (newValue) {
+      localStorage.setItem("email", user.email);
+      localStorage.setItem("password", user.password);
+    } else {
+      localStorage.removeItem("email");
+      localStorage.removeItem("password");
+    }
+  };
 
-        if (name === 'password') {
-            setCountPass(value.length < 8)
-        }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        setUser((prevUser) => ({
-            ...prevUser,
-            [name]: value,
-        }))
+    if (!user.email.trim() || !user.password.trim()) {
+      return window.alert("Email y contraseña obligatorios.");
     }
 
-    const handleToggleChange = (newValue) => {
-        setRememberPassword(newValue)
-
-        if (newValue) {
-            localStorage.setItem('email', user.email)
-            localStorage.setItem('password', user.password)
-        } else {
-            localStorage.removeItem('email')
-            localStorage.removeItem('password')
-        }
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    if (!emailRegex.test(user.email)) {
+      return window.alert("El formato del email no es correcto!");
     }
 
-    const handleSubmit = async (event) => {
-        event.preventDefault()
-    
-        if (!user.email.trim() || !user.password.trim()) {
-            return window.alert('Email y contraseña obligatorios.')
-        }
-    
-        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
-        if (!emailRegex.test(user.email)) {
-            return window.alert('El formato del email no es correcto!')
-        }
-    
-        try {
-            const response = await fetch(ENDPOINT, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(user),
-            })
-    
-            if (!response.ok) {
-                const errorData = await response.json()
-                const message = errorData.message || 'Ocurrió un error con la solicitud.'
-                return window.alert(`${message} 🙁.`)
-            }
-    
-            const data = await response.json()
-            window.sessionStorage.setItem('token', data.token)
-            window.alert('Usuario identificado con éxito.')
-            navigate('/perfil')
+    try {
+      const response = await fetch(ENDPOINT, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
 
-        } catch (error) {
-            console.error(error)
-            window.alert('No se pudo conectar con el servidor. Por favor, intenta más tarde.')
-        }
+      if (!response.ok) {
+        const errorData = await response.json();
+        const message =
+          errorData.message || "Ocurrió un error con la solicitud.";
+        return window.alert(`${message} 🙁.`);
+      }
+
+      const data = await response.json();
+      window.sessionStorage.setItem("token", data.token);
+      window.alert("Usuario identificado con éxito.");
+      navigate("/perfil");
+    } catch (error) {
+      console.error(error);
+      window.alert(
+        "No se pudo conectar con el servidor. Por favor, intenta más tarde."
+      );
     }
+  };
 
-    const goToForgotPassword = () => {
-        navigate('/forgot-password')
-    }
+  const goToForgotPassword = () => {
+    navigate("/forgot-password");
+  };
 
-    const goToSignUp = () => {
-        navigate('/signup')
-    }
+  const goToSignUp = () => {
+    navigate("/signup");
+  };
 
-    return (
-        <div className="w-[287px] h-[567px] flex flex-col gap-4 p-4 items-center">
-            <h1 className="text-center text-xl font-workSans font-semibold italic">Te damos la bienvenida!</h1>
-            <p className="text-center text-sm font-roboto">Por favor ingresa tus datos</p>
-            <form 
-                className="flex flex-col gap-1"
-                onSubmit={handleSubmit}
-            > 
-                <p className="text-sm font-roboto">Correo electrónico</p>
-                <input
-                    className="w-full px-1 py-1 border-2 border-gray-300 rounded-md"
-                    type="email"
-                    name="email"
-                    value={user.email}
-                    onChange={handleInputChange}
-                    placeholder="correo@mail.com"
-                />
-                <p className="text-sm font-roboto">Contraseña</p>
-                <input
-                    className={`w-full px-1 py-1 border-2 rounded-md ${rememberPassword ? 'border-primary-500' : 'border-gray-300'}`}
-                    type="password"
-                    name="password"
-                    value={user.password}
-                    onChange={handleInputChange}
-                    placeholder="Ingresa tu contraseña"
-                />
-                {countPass && (<p className="text-xs text-gray-500">Mínimo 8 caracteres</p>)}
-                <div className="flex items-center gap-2">
-                    <ToggleSwitch enabled={rememberPassword} onChange={handleToggleChange} />
-                </div>
-                <button 
-                    type="submit"
-                    className="w-full h-8 mt-4 bg-primary-500 text-white font-workSans font-semibold text-sm px-4 rounded-full hover:bg-primary-700 focus:outline-none focus:ring-2  focus:ring-opacity-50" 
-                >
-                    Ingresar
-                </button> 
-                <div className="flex flex-col gap-3">
-                    <p onClick={goToForgotPassword} className="text-primary-500 text-center text-xs cursor-pointer">Olvidé mi contraseña</p>
-                    <div>
-                        <p className="text-center text-sm">¿No tienes una cuenta? <span onClick={goToSignUp} className="font-medium font-workSans
-                         cursor-pointer text-primary-500">Regístrate</span> </p>
-                    </div>
-                </div>
-            </form>
+  return (
+    <div className="w-[287px] h-[567px] flex flex-col gap-4 p-4 items-center">
+      <h1 className="text-center text-xl font-workSans font-semibold italic">
+        Te damos la bienvenida!
+      </h1>
+      <p className="text-center text-sm font-roboto">
+        Por favor ingresa tus datos
+      </p>
+      <form className="flex flex-col gap-1" onSubmit={handleSubmit}>
+        <p className="text-sm font-roboto">Correo electrónico</p>
+        <input
+          className="w-full px-1 py-1 border-2 border-gray-300 rounded-md"
+          type="email"
+          name="email"
+          value={user.email}
+          onChange={handleInputChange}
+          placeholder="correo@mail.com"
+        />
+        <p className="text-sm font-roboto">Contraseña</p>
+        <input
+          className={`w-full px-1 py-1 border-2 rounded-md ${
+            rememberPassword ? "border-primary-500" : "border-gray-300"
+          }`}
+          type="password"
+          name="password"
+          value={user.password}
+          onChange={handleInputChange}
+          placeholder="Ingresa tu contraseña"
+        />
+        {countPass && (
+          <p className="text-xs text-gray-500">Mínimo 8 caracteres</p>
+        )}
+        <div className="flex items-center gap-2">
+          <ToggleSwitch
+            enabled={rememberPassword}
+            onChange={handleToggleChange}
+          />
         </div>
-    )
-}
+        <button
+          type="submit"
+          className="w-full h-8 mt-4 bg-primary-500 text-white font-workSans font-semibold text-sm px-4 rounded-full hover:bg-primary-700 focus:outline-none focus:ring-2  focus:ring-opacity-50"
+        >
+          Ingresar
+        </button>
+        <div className="flex flex-col gap-3">
+          <p
+            onClick={goToForgotPassword}
+            className="text-primary-500 text-center text-xs cursor-pointer"
+          >
+            Olvidé mi contraseña
+          </p>
+          <div>
+            <p className="text-center text-sm">
+              ¿No tienes una cuenta?{" "}
+              <span
+                onClick={goToSignUp}
+                className="font-medium font-workSans
+                         cursor-pointer text-primary-500"
+              >
+                Regístrate
+              </span>{" "}
+            </p>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/components/register/FormRegister.jsx
+++ src/components/register/FormRegister.jsx
@@ -1,175 +1,271 @@
-import { useForm } from 'react-hook-form'
-import { useNavigate } from 'react-router-dom'
-import ArrowBackButton from '../_common/ArrowBackButton'
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import ArrowBackButton from "../_common/ArrowBackButton";
 
 export const FormRegister = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const ENDPOINT = "http://localhost:3000/usuario/newUsuario";
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleLoginRedirect = () => {
-    navigate('/login')
-  }
+    navigate("/login");
+  };
 
-  const onSubmit = (data) => {
-    console.log(data)
-  }
+  const onSubmit = async (event) => {
+    if (!event.email.trim() || !event.password.trim()) {
+      return window.alert("Email y contraseña obligatorios.");
+    }
+
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    if (!emailRegex.test(event.email)) {
+      return window.alert("El formato del email no es correcto!");
+    }
+
+    try {
+      const response = await fetch(ENDPOINT, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(event),
+      });
+      console.log(response);
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        const message =
+          errorData.message || "Ocurrió un error con la solicitud.";
+        return window.alert(`${message} 🙁.`);
+      } else {
+        handleLoginRedirect();
+      }
+    } catch (error) {
+      console.error(error);
+      window.alert(
+        "No se pudo conectar con el servidor. Por favor, intenta más tarde."
+      );
+    }
+  };
 
   return (
-    <div className='w-full flex justify-center mt-10 lg:mt-0'>
+    <div className="w-full flex justify-center mt-10 lg:mt-0">
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className='bg-gray-100 rounded-lg w-[287px] p-4 tablet:w-[730px] desktop:w-[1084px] shadow-lg'
+        className="bg-gray-100 rounded-lg w-[287px] p-4 tablet:w-[730px] desktop:w-[1084px] shadow-lg"
         style={{
           backgroundImage:
             'url("/src/assets/images/bg2.png"), linear-gradient(rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.3))',
-          backgroundBlendMode: 'overlay'
+          backgroundBlendMode: "overlay",
         }}
       >
         {/*componente de boton flecha < goback */}
-        <ArrowBackButton/>
+        <ArrowBackButton />
 
-        <h2 className='font-workSans italic text-2xl font-semibold mb-4 text-center'>
+        <h2 className="font-workSans italic text-2xl font-semibold mb-4 text-center">
           Te damos la Bienvenida!
         </h2>
-        <p className='text-center mb-6'>
+        <p className="text-center mb-6">
           Regístrate e inicia tu transformación laboral
         </p>
 
-        <div className='grid grid-cols-1 tablet:grid-cols-2 gap-4'>
+        <div className="grid grid-cols-1 tablet:grid-cols-2 gap-4">
           <div>
-            <label htmlFor='firstName' className='block font-medium'>
+            <label htmlFor="firstName" className="block font-medium">
               Nombre *
             </label>
             <input
-              type='text'
-              name='firstName'
-              {...register('firstName', { required: 'Nombre es requerido' })}
-              className='mt-1 block w-full p-2 border border-gray-300 rounded-md'
-              placeholder='Nombres'
+              type="text"
+              name="firstName"
+              {...register("firstName", { required: "Nombre es requerido" })}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+              placeholder="Nombres"
             />
-            {errors.firstName && <p className='text-red-500 text-sm'>{errors.firstName.message}</p>}
+            {errors.firstName && (
+              <p className="text-red-500 text-sm">{errors.firstName.message}</p>
+            )}
           </div>
           <div>
-            <label htmlFor='lastName' className='block font-medium'>
+            <label htmlFor="lastName" className="block font-medium">
               Apellido *
             </label>
             <input
-              type='text'
-              name='lastName'
-              {...register('lastName', { required: 'Apellido es requerido' })}
-              className='mt-1 block w-full p-2 border border-gray-300 rounded-md'
-              placeholder='Apellidos'
+              type="text"
+              name="lastName"
+              {...register("lastName", { required: "Apellido es requerido" })}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+              placeholder="Apellidos"
             />
-            {errors.lastName && <p className='text-red-500 text-sm'>{errors.lastName.message}</p>}
+            {errors.lastName && (
+              <p className="text-red-500 text-sm">{errors.lastName.message}</p>
+            )}
           </div>
           <div>
-            <label htmlFor='email' className='block font-medium'>
+            <label htmlFor="email" className="block font-medium">
               Correo electrónico *
             </label>
             <input
-              type='email'
-              name='email'
-              {...register('email', { required: 'Correo electrónico es requerido' })}
-              className='mt-1 block w-full p-2 border border-gray-300 rounded-md'
-              placeholder='ejemplo@gmail.com'
+              type="email"
+              name="email"
+              {...register("email", {
+                required: "Correo electrónico es requerido",
+              })}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+              placeholder="ejemplo@gmail.com"
             />
-            {errors.email && <p className='text-red-500 text-sm'>{errors.email.message}</p>}
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
+            )}
           </div>
           <div>
-            <label htmlFor='country' className='block font-medium'>
-              País *
+            <label htmlFor="country" className="block font-medium">
+              País
             </label>
             <select
-              name='country'
-              {...register('country', { required: 'País es requerido' })}
-              className='mt-1 block w-full p-2 border border-gray-300 rounded-md'
+              name="country"
+              {...register("country", { required: "País es requerido" })}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
             >
-              <option value=''>País</option>
-              <option value='mx'>México</option>
-              <option value='ar'>Argentina</option>
-              <option value='ven'>Venezuela</option>
-              <option value='cl'>Chile</option>
-              <option value=''>Otro</option>
+              <option value="">País</option>
+              <option value="México">México</option>
+              <option value="Argentina">Argentina</option>
+              <option value="Venezuela">Venezuela</option>
+              <option value="Chile">Chile</option>
+              <option value="Perú">Perú</option>
+              <option value="Uruguay">Uruguay</option>
+              <option value="Bolivia">Bolivia</option>
+              <option value="España">España</option>
+              <option value="Otro">Otro</option>
             </select>
-            {errors.country && <p className='text-red-500 text-sm'>{errors.country.message}</p>}
+            {errors.country && (
+              <p className="text-red-500 text-sm">{errors.country.message}</p>
+            )}
           </div>
           <div>
-            <label htmlFor='education' className='block font-medium'>
+            <label htmlFor="education" className="block font-medium">
               Educación *
             </label>
-            <input
-              type='text'
-              name='education'
-              {...register('education', { required: 'Educación es requerida' })}
-              className='mt-1 block w-full p-2 border border-gray-300 rounded-md'
-              placeholder='Educación'
-            />
-            {errors.education && <p className='text-red-500 text-sm'>{errors.education.message}</p>}
+            <select
+              name="education"
+              {...register("education", { required: "Educación es requerido" })}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+            >
+              <option value="">Educación</option>
+              <option value="Desarrollador Fullstack">
+                Desarrollador Fullstack
+              </option>
+              <option value="Desarrollador Frontend">
+                Desarrollador Frontend
+              </option>
+              <option value="Desarrollador Backend">
+                Desarrollador Backend
+              </option>
+              <option value="DevOps">DevOps</option>
+              <option value="Ingeniero Informático">
+                Ingeniero Informático
+              </option>
+              <option value="Ingeniero en Computación">
+                Ingeniero en Computación
+              </option>
+              <option value="Analista de Sistemas">Analista de Sistemas</option>
+              <option value="Ingeniero de Sistemas">
+                Ingeniero de Sistemas
+              </option>
+              <option value="Otro">Otro</option>
+            </select>
+            {errors.education && (
+              <p className="text-red-500 text-sm">{errors.education.message}</p>
+            )}
           </div>
           <div>
-            <label htmlFor='jobTitle' className='block font-medium'>
+            <label htmlFor="jobTitle" className="block font-medium">
               Cargo *
             </label>
             <select
-              name='jobTitle'
-              {...register('jobTitle', { required: 'Cargo es requerido' })}
-              className='mt-1 block w-full p-2 border border-gray-300 rounded-md'
+              name="jobTitle"
+              {...register("jobTitle", { required: "Cargo es requerido" })}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
             >
-              <option value=''>Cargo</option>
-              <option value='manager'>Gerente</option>
-              <option value='Ingeniero'>Ing. de Sistema</option>
-              <option value='developer'>Desarrollador Fullstack</option>
-              <option value='developer'>Desarrollador Frontend</option>
-              <option value='developer'>Desarrollador Backend</option>
-              <option value='other'>Otro</option>
+              <option value="">Cargo</option>
+              <option value="Desarrollador">Desarrollador</option>
+              <option value="Diseñador">Diseñador</option>
+              <option value="Analista">Analista</option>
+              <option value="Otro">Otro</option>
             </select>
-            {errors.jobTitle && <p className='text-red-500 text-sm'>{errors.jobTitle.message}</p>}
+            {errors.jobTitle && (
+              <p className="text-red-500 text-sm">{errors.jobTitle.message}</p>
+            )}
           </div>
           <div>
-            <label htmlFor='experience' className='block font-medium'>
+            <label htmlFor="experience" className="block font-medium">
               Experiencia *
             </label>
-            <input
-              type='text'
-              name='experience'
-              {...register('experience', { required: 'Experiencia es requerida' })}
-              className='mt-1 block w-full p-2 border border-gray-300 rounded-md'
-              placeholder='Cargo'
-            />
-            {errors.experience && <p className='text-red-500 text-sm'>{errors.experience.message}</p>}
+            <select
+              name="jobTitle"
+              {...register("experience", {
+                required: "Experiencia es requerido",
+              })}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+            >
+              <option value="">Experiencia</option>
+              <option value="Sin Experiencia">sin experiencia</option>
+              <option value="Trainee">1 año</option>
+              <option value="Semi-Senior">2 a 4 años</option>
+              <option value="Senior">5 o más</option>
+            </select>
+            {errors.experience && (
+              <p className="text-red-500 text-sm">
+                {errors.experience.message}
+              </p>
+            )}
           </div>
           <div>
-            <label htmlFor='password' className='block font-medium'>
+            <label htmlFor="password" className="block font-medium">
               Crear Contraseña *
             </label>
             <input
-              type='password'
-              name='password'
-              {...register('password', { required: 'Contraseña es requerida', minLength: { value: 8, message: 'Mínimo 8 caracteres' } })}
-              className='mt-1 block w-full p-2 border border-gray-300 rounded-md'
-              placeholder='Crea una contraseña'
+              type="password"
+              name="password"
+              {...register("password", {
+                required: "Contraseña es requerida",
+                minLength: { value: 8, message: "Mínimo 8 caracteres" },
+                pattern: {
+                  value: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$/,
+                  message:
+                    "Debe contener al menos una letra mayúscula, una minúscula y un número",
+                },
+              })}
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+              placeholder="Crea una contraseña"
             />
-            {errors.password && <p className='text-red-500 text-sm'>{errors.password.message}</p>}
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password.message}</p>
+            )}
           </div>
         </div>
 
-        <div className='mt-6'>
+        <div className="mt-6">
           <button
-            type='submit'
-            className='w-full bg-[#729e2e] text-white py-2 px-4 rounded-md hover:bg-[#89b049] transition duration-300'
+            type="submit"
+            className="w-full bg-[#729e2e] text-white py-2 px-4 rounded-md hover:bg-[#89b049] transition duration-300"
           >
             Crear cuenta
           </button>
         </div>
 
-        <div className='mt-4 text-center'>
+        <div className="mt-4 text-center">
           <p>
-            ¿Ya tienes cuenta?{' '}
+            ¿Ya tienes cuenta?{" "}
             <button onClick={handleLoginRedirect}>Inicia Sesión </button>
           </p>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
+
+export default FormRegister;
