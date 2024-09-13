168c6a873d048ccbf045b0c3b533f9653b46a1a8
fernandosbenavides <fernandosbenavides@gmail.com>
2024-09-12 22:03:57 -0300

    fix de conflictos

--- src/routes/RoutesApp.jsx
+++ src/routes/RoutesApp.jsx
@@ -5,6 +5,8 @@ import ProfilePage from "../pages/profile/ProfilePage.jsx";
 import NotificationPage from "../pages/NotificationPage.jsx";
 import SignupPage from "../pages/SignupPage.jsx";
 import ForgotPasswordPage from "../pages/ForgotPasswordPage.jsx";
+import GoalsPage from '../pages/GoalsPage.jsx'
+import TaskTablePage from "../pages/TaskTablePage.jsx";
 import MetricsPage from "../pages/Metrics.jsx";
 
 const RoutesApp = () => {
@@ -17,6 +19,8 @@ const RoutesApp = () => {
 			<Route path="/signup" element={<SignupPage/>} />
 			<Route path="/notification" element={<NotificationPage/>} />
 			<Route path="/forgot-password" element={<ForgotPasswordPage/>} />
+			<Route path='/goals' element={<GoalsPage/>} />
+			<Route path="/tasks" element={<TaskTablePage/>}></Route>
 			<Route path="/metrics" element={<MetricsPage/>} />
 		</Routes>
 	);
