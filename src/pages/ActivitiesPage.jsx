aad3b9bcdb9818d25ebefc9061d59c6de9a62908
Manuel.Millan1979 <nutriglar@gmail.com>
2024-11-02 16:10:49 -0300

    ActivitiesPage

--- src/pages/ActivitiesPage.jsx
+++ src/pages/ActivitiesPage.jsx
@@ -4,27 +4,26 @@ import { CiUser } from 'react-icons/ci'
 import SidebarComponent from '../components/Navbar/Sidebar'
 
 const Activities = () => {
-  const [users, setUsers] = useState([])
+  const [activities, setActivities] = useState([])
+  const [error, setError] = useState(null); // Se añadió el estado error
   const ENDPOINT = './FakeDataActivities.json'
 
-  const dateRange = 'Lunes 03 junio, 2024 - Lunes 10 junio, 2024'
-
   useEffect(() => {
-    const FetchData = async () => {
+    const fetchData = async () => {
       try {
         const response = await fetch(ENDPOINT)
         if (!response.ok) {
           throw new Error('Error al obtener los datos')
         }
         const data = await response.json()
-        setUsers(data)
+        setActivities(data)
       } catch (error) {
         setError(error.message)
         console.error(error)
       }
     }
 
-    FetchData()
+    fetchData()
   }, [])
 
   const GetColorByCategorie = (categorie) => {
@@ -46,73 +45,53 @@ const Activities = () => {
         <SidebarComponent />
       </div>
       <div className='h-screen w-full flex flex-col p-8 ml-16 sm:ml-24'>
-        <h1 className='text-center font-bold italic text-3xl text-primary-500'>Actividades solo para creación de archivo</h1>
-        <div className='w-full flex items-center justify-between pt-6'>
-          <div className='flex items-center gap-2'>
-            <button className='h-[34px] flex items-center  font-medium border-2 border-gray-300 rounded-md px-3 py-2'>
-              <CiUser className='h-[16px] text-500' />
-              <p>Tutor Asignado</p>
-            </button>
-            <button className='h-[34px] flex items-center text-sm font-medium border-2 border-gray-300 rounded-md px-3 py-2'>
-              Categoría
-            </button>
+        <h1 className='text-left font-bold text-2xl mb-4'>Actividades</h1>
+        
+        {/* Filtros */}
+        <div className='flex justify-between items-center'>
+          <div className='flex space-x-2'>
+            <button className='px-4 py-1 bg-green-200 text-green-700 rounded'>Todo</button>
+            <button className='px-4 py-1 border border-gray-300 text-gray-500 rounded'>15 Postulaciones</button>
+            <button className='px-4 py-1 border border-gray-300 text-gray-500 rounded'>5 Entrevistas</button>
+            <button className='px-4 py-1 border border-gray-300 text-gray-500 rounded'>2 Eventos de Networking</button>
           </div>
-          <p className='text-sm px-3 py-2 border-2 border-gray-300 flex gap-2 items-center justify-between'>
-            <FaChevronLeft className='h-[14px] text-500 cursor-pointer' />
-            {dateRange}
-            <FaChevronLeft className='h-[14px] text-500 cursor-pointer rotate-180' />
-          </p>
+          <button className='bg-green-500 text-white px-4 py-2 rounded-full'>Objetivo</button>
         </div>
-        <div className='flex flex-row pt-6 w-full'>
-          <div>
-            <table>
-              <thead>
-                <tr>
-                  <th className='w-[329px] flex justify-start border-r-2 border-r-black border-b-2 border-b-gray-300 pl-2 items-center text-xl'>Usuario</th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user, index) => (
-                  <td key={index} className='w-[329px] flex justify-start items-center pl-3 py-3'>
-                    <input type='checkbox' className='mr-2 accent-primary-500' />
-                    <p className='flex items-center py-2'>{user.name} {user.lastName}</p>
-                  </td>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <div className='overflow-x-auto h-full'>
-            <table className='min-w-full whitespace-nowrap h-[450px]'>
-              <thead>
-                <tr className='flex items-center'>
-                  <th className='w-[180px] flex items-center justify-start border-r-2 border-r-black border-b-2 border-b-gray-300 pl-2 text-xl'>Categoría</th>
-                  <th className='w-[210px] flex items-center justify-start border-r-2 border-r-black border-b-2 border-b-gray-300 pl-2 text-xl'>Título</th>
-                  <th className='w-[150px] flex items-center justify-start border-r-2 border-r-black border-b-2 border-b-gray-300 pl-2 text-xl'>Completado</th>
-                  <th className='w-[325px] flex items-center justify-start border-r-2 border-r-black border-b-2 border-b-gray-300 pl-2 text-xl'>Título profesional</th>
-                  <th className='w-[325px] flex items-center justify-start border-r-2 border-r-black border-b-2 border-b-gray-300 pl-2 text-xl'>Compañía</th>
-                  <th className='w-[325px] flex items-center justify-start border-r-2 border-r-black border-b-2 border-b-gray-300 pl-2 text-xl'>Fecha Inicio</th>
-                  <th className='w-[325px] flex items-center justify-start border-r-2 border-r-black border-b-2 border-b-gray-300 pl-2 text-xl'>Fecha Creación</th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user, index) => (
-                  <tr key={index} className='flex items-center'>
-                    <td className='w-[180px] flex items-center justify-start text-white  pl-3 py-3 text-base'><p className={`rounded-lg p-2 ${GetColorByCategorie(user.categorie)}`}>{user.categorie}</p></td>
-                    <td className='w-[210px] flex items-center justify-start  pl-3 py-3 text-base'>{user.title}</td>
-                    <td className='w-[150px] flex items-center justify-center  pl-3 py-3 text-base'><input className='accent-primary-500' type='checkbox' /></td>
-                    <td className='w-[325px] flex items-center justify-start  pl-3 py-3 text-base'>{user.professionalTitle}</td>
-                    <td className='w-[325px] flex items-center justify-start  pl-3 py-3 text-base'>{user.company}</td>
-                    <td className='w-[325px] flex items-center justify-start  pl-3 py-3 text-base'>{user.dateStart}</td>
-                    <td className='w-[325px] flex items-center justify-start  pl-3 py-3 text-base'>{user.dateCreation}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <button className='absolute bottom-20 right-6 font-workSans font-semibold text-sm text-primary-500 px-4 py-2 border-2 border-gray-300 rounded-full'>
-            Descargar
-          </button>
+
+        {/* Tabla */}
+        <div className='mt-6 overflow-x-auto'>
+        <table className='min-w-full whitespace-nowrap h-[450px]'>
+  <thead>
+    <tr className='flex items-center'>
+      <th className='w-[180px] flex items-center justify-start border-r-2 border-r-gray-700 border-b-2 border-b-gray-300 pl-2 text-xl'>Categoría</th>
+      <th className='w-[210px] flex items-center justify-start border-r-2 border-r-gray-700 border-b-2 border-b-gray-300 pl-2 text-xl'>Título</th>
+      <th className='w-[150px] flex items-center justify-start border-r-2 border-r-gray-700 border-b-2 border-b-gray-300 pl-2 text-xl'>Completado</th>
+      <th className='w-[325px] flex items-center justify-start border-r-2 border-r-gray-700 border-b-2 border-b-gray-300 pl-2 text-xl'>Título profesional</th>
+      <th className='w-[325px] flex items-center justify-start border-r-2 border-r-gray-700 border-b-2 border-b-gray-300 pl-2 text-xl'>Compañía</th>
+      <th className='w-[325px] flex items-center justify-start border-r-2 border-r-gray-700 border-b-2 border-b-gray-300 pl-2 text-xl'>Fecha Inicio</th>
+      <th className='w-[325px] flex items-center justify-start border-r-2 border-r-gray-700 border-b-2 border-b-gray-300 pl-2 text-xl'>Fecha Creación</th>
+    </tr>
+  </thead>
+  <tbody>
+    {users.map((user, index) => (
+      <tr key={index} className='flex items-center'>
+        <td className='w-[180px] flex items-center justify-start text-white pl-3 py-3 text-base'><p className={`rounded-lg p-2 ${GetColorByCategorie(user.categorie)}`}>{user.categorie}</p></td>
+        <td className='w-[210px] flex items-center justify-start border-r-2 border-r-gray-700 pl-3 py-3 text-base'>{user.title}</td>
+        <td className='w-[150px] flex items-center justify-center border-r-2 border-r-gray-700 pl-3 py-3 text-base'><input className='accent-primary-500' type='checkbox' /></td>
+        <td className='w-[325px] flex items-center justify-start border-r-2 border-r-gray-700 pl-3 py-3 text-base'>{user.professionalTitle}</td>
+        <td className='w-[325px] flex items-center justify-start border-r-2 border-r-gray-700 pl-3 py-3 text-base'>{user.company}</td>
+        <td className='w-[325px] flex items-center justify-start border-r-2 border-r-gray-700 pl-3 py-3 text-base'>{user.dateStart}</td>
+        <td className='w-[325px] flex items-center justify-start pl-3 py-3 text-base'>{user.dateCreation}</td>
+      </tr>
+    ))}
+  </tbody>
+</table>
+
         </div>
+
+        <button className='fixed bottom-10 right-10 bg-primary-500 text-white px-4 py-2 rounded-full'>
+          Descargar
+        </button>
       </div>
     </div>
   )
